d554ca6b600f21fc9b3a6e96d5618b7ce4655f13
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-05-27 00:04:19 +0600

    Custom Hook used and Each Category Display Done

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,12 +1,29 @@
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg'
+import useMenu from '../../../hooks/useMenu';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import MenuCategory from '../MenuCategory/MenuCategory';
+
 
 const Menu = () => {
+    const [menu] = useMenu();
+    const dessert = menu.filter(item => item.category === "dessert");
+    const salad = menu.filter(item => item.category === "salad");
+    const soup = menu.filter(item => item.category === "soup");
+    const pizza = menu.filter(item => item.category === "pizza");
+    const offered = menu.filter(item => item.category === "offered");
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <h2>Menu</h2>
+            <Cover img={menuImg} title="our menu"></Cover>
+            <SectionTitle
+                subHeading="Don't miss"
+                heading="Today's Offer"
+            ></SectionTitle>
+            <MenuCategory items={offered}></MenuCategory>
         </div>
     );
 };
